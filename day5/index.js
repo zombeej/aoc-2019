2f6d392180d468cfa00f66461b9ef299b08b0a67
Beejay Morgan <beejay@simplecast.com>
2019-12-05 02:07:01 -0500

    💻 Prompt for the system ID

--- day5/index.js
+++ day5/index.js
@@ -4,9 +4,15 @@ const data = require('./data').split(',').map(o => parseInt(o))
 // 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
 // 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99`.split(',').map(o => parseInt(o))
 
-const sysID = 5
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-async function computer (ops, o = 0) {
+// const sysID = 1
+
+function computer (ops, o = 0) {
   const ck = ops[o]
   let op = ck
   let modes = []
@@ -21,49 +27,54 @@ async function computer (ops, o = 0) {
     ops[ops[o + 3]] = p1 + p2
     return computer(ops, o + 4)
   }
-  if (op === 2) {
+  else if (op === 2) {
     const p1 = modes[0] ? ops[o + 1] : ops[ops[o + 1]]
     const p2 = modes[1] ? ops[o + 2] : ops[ops[o + 2]]
     ops[ops[o + 3]] = p1 * p2
     return computer(ops, o + 4)
   }
-  if (op === 3) {
-    ops[ops[o + 1]] = sysID
-    return computer(ops, o + 2)
+  else if (op === 3) {
+    rl.question('Enter system ID:', sysId => {
+      rl.close()
+      sysId = parseInt(sysId)
+      ops[ops[o + 1]] = sysId
+      computer(ops, o + 2)
+    })
   }
-  if (op === 4) {
+  else if (op === 4) {
     const p1 = modes[0] ? ops[o + 1] : ops[ops[o + 1]]
     console.log(`Instruction ${o}:`, p1)
     return computer(ops, o + 2)
   }
-  if (op === 5) {
+  else if (op === 5) {
     const p1 = modes[0] ? ops[o + 1] : ops[ops[o + 1]]
     const p2 = modes[1] ? ops[o + 2] : ops[ops[o + 2]]
     const next = p1 !== 0 ? p2 : o + 3
     return computer(ops, next)
   }
-  if (op === 6) {
+  else if (op === 6) {
     const p1 = modes[0] ? ops[o + 1] : ops[ops[o + 1]]
     const p2 = modes[1] ? ops[o + 2] : ops[ops[o + 2]]
     const next = p1 === 0 ? p2 : o + 3
     return computer(ops, next)
   }
-  if (op === 7) {
+  else if (op === 7) {
     const p1 = modes[0] ? ops[o + 1] : ops[ops[o + 1]]
     const p2 = modes[1] ? ops[o + 2] : ops[ops[o + 2]]
     ops[ops[o + 3]] = p1 < p2 ? 1 : 0
     return computer(ops, o + 4)
   }
-  if (op === 8) {
+  else if (op === 8) {
     const p1 = modes[0] ? ops[o + 1] : ops[ops[o + 1]]
     const p2 = modes[1] ? ops[o + 2] : ops[ops[o + 2]]
     ops[ops[o + 3]] = p1 === p2 ? 1 : 0
     return computer(ops, o + 4)
   }
-  if (op === 99) {
-    return ops
+  else if (op === 99) {
+    console.log('Program complete')
+  } else {
+    console.log(`bad op code ${ops[o]}`)
   }
-  return `bad op code ${ops[o]}`
 }
 
 computer(data)
